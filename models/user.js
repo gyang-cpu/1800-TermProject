b28f9bf36af011d4e426d6be8cd7147cb94d0884
Daniel Liu <daniel.sc.liu@gmail.com>
2020-03-24 16:54:11 -0700

    salting and hashing

--- models/user.js
+++ models/user.js
@@ -1,10 +1,56 @@
 const mongoose = require("mongoose");
 const passportMongoose = require("passport-local-mongoose");
 
+//validates that no duplicate sign ups // 
+const uniqueValidator = require('mongoose-unique-validator');
+const crypto = require('crypto');
+
+const jwt = require('jsonwebtoken');
+var secret = require('../config').secret;
+
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
+    username: {type: String, required: [true, "username cannot be blank"]}
+    
 });
+
+UserSchema.plugin(uniqueValidator, {message:'please choose another username this is already taken'});
+
+//// -------> we can probably insert this as a method inside of userSchema
+/// store the password as an attribute in the schema //  
+UserSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+}
+
+//-->Function to validate the hashed passwords stored in Mongo
+UserSchema.methods.validatePassword = function(password){
+    let hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash
+}
+
+//--> generates a JWT token for session control. 
+// exp.setdate sets the length of time user will be kicked off the session
+UserSchema.methods.generateJWT = function() {
+      let today = new Date();
+      let exp = new Date(today);
+      exp.setDate(today.getDate() + 30);
+    
+      return jwt.sign({
+        id: this._id,
+        username: this.username,
+        exp: parseInt(exp.getTime() / 1000),
+      }, secret);
+    };
+    
+//---> 
+UserSchema.methods.toAuthJSON = function(){
+      return {
+        username: this.username,
+        token: this.generateJWT(),
+        
+        };
+};
+    
 
 UserSchema.plugin(passportMongoose);
 
